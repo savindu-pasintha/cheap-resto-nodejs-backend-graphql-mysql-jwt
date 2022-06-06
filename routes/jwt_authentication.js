d29ac364890c2e288532eee31ed9550e8e1e8d9c
savindu-pasintha <savindupasingtha@gmail.com>
2022-06-06 08:56:23 +0530

    by savindu

--- routes/jwt_authentication.js
+++ routes/jwt_authentication.js
@@ -4,30 +4,53 @@ var jwt = require('jsonwebtoken');
 const { MySqlQueryExecute } = require('./mysq_database');
 
 //https://www.npmjs.com/package/jsonwebtoken
+// MySqlQueryExecute("CREATE TABLE user (id INT.name VARCHAR(255), address VARCHAR(255))");
 
 router.get('/login', function (req, res, next) {
-  MySqlQueryExecute("CREATE TABLE user (id INT.name VARCHAR(255), address VARCHAR(255))");
-  res.send('Login with JWT');
+   res.send({'message':'Login with JWT','url':'http://localhost:5000/jwt/login'});
 });
 
 
-//new Token create for user data encode x.y.z   x=header y=payload data z=encript type
-router.post('/login/createToken', function (req, res, next) {
-  var userData = { id: 1, username: "savindupasingtha@gmail.com", password: "pass99" };
-  jwt.sign({
+/*new Token create for user data
+ encode x.y.z   x=header{
+  "alg": "HS256",
+  "typ": "JWT"
+} y=payload_data
+{username:dsavindu}
+ z=encript type
+*/
+//bs64 to  value = atob(bs64string)
+const allTokensArray = [];
+router.post('/login/create-token', function (req, res, next) {
+  var userData = req?.body ? req.body : {
+    "role":"admin",
+    "username":"savindu",
+    "password":"12345",
+    "id": Math.round(Math.random(100000))
+};
+ var expireTimeOneHour = Math.floor(Date.now() / 1000) + (60 * 60);
+ allTokensArray.push(userData); 
+ jwt.sign({
     user: userData,
-    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    exp:expireTimeOneHour ,
   }, "secret", (err, token) => {
     if (err) { res.json({ error: err }); }
     else {
-      res.json({ token: token, exp: Math.floor(Date.now() / 1000) + (60 * 60), });
+      res.json({ 
+      token: token, 
+      exp: expireTimeOneHour, 
+      expActualDate: new Date(expireTimeOneHour) 
+    });
     }
   });
+
 });
 
 //http header include token is validation
 const verifyTokenFuc = (req, res, next) => {
+  console.log("gggg",res.token);
   if (typeof (req.headers['authorization']) !== 'undefined') {
+    console.log(req.headers);
     var headerToken = req.headers['authorization'].split(" ")[1];
     if (headerToken != null && headerToken != 'undefined') {
       req.token = headerToken; next();
@@ -36,25 +59,15 @@ const verifyTokenFuc = (req, res, next) => {
   } else { res.json({ error: "UnAuthorization Request !" }) }
 }
 
-//after validation , jwt.verify using decode token inside data ana out put 
-router.post('/login/verifyToken', verifyTokenFuc, function (req, res) {
-  jwt.verify(req.token, "secret", (err, getData) => {
+//after validation , jwt.verify using decode token inside data and out put 
+router.post('/login/verify-token', verifyTokenFuc, function (req, res) {
+  console.log(res.body);
+  jwt.verify(res.token, "secret", (err, getData) => {
     if (err) { res.json({ msg: "Access Denied !", error: err }); }
     else { res.json({ success: true, msg: " Token Verified By JWT .", data: getData }); }
   });
 });
 
 
-router.post('/register', function (req, res, next) {
-  res.send('');
-});
-
-router.post('/logi', function (req, res, next) {
-  res.send('respond with a resource - users GET');
-});
-
-router.post('/logout', function (req, res, next) {
-  res.send('respond with a resource - users GET');
-});
 
 module.exports = router;
